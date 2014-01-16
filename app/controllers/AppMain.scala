4e6bfa6bbe28480a0cf92f9d217afffe63f6e701
steppenwells <steppenwells@gmail.com>
2014-01-16 12:50:52 +0000

    hello is now templated.
    with bootstrap and the like to make it look familiar

--- app/controllers/AppMain.scala
+++ app/controllers/AppMain.scala
@@ -5,6 +5,6 @@ import play.api.mvc.{Action, Controller}
 object AppMain extends Controller {
 
   def welcome() = Action {
-    Ok("hello world")
+    Ok(views.html.Application.welcome("simple message..."))
   }
 }
